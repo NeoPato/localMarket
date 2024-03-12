08992fa4a864cc6cbfa2898ea2677c441515ce7a
Csharpedo <117386614+Csharpedo@users.noreply.github.com>
2024-03-12 16:36:31 -0300

    Cosa

---
+++ localMarket/Login/script.js
@@ -0,0 +1,35 @@
+function loginClick(){
+    var name = $('#user').val();
+    var password = $('#password').val();
+
+    if(name && password && name === "admin" && password === "admin"){
+        const user = {
+            name: name,
+            entryDate: new Date(),
+            id: Math.floor(Math.random() * 100000),
+        }
+        localStorage.setItem("user", JSON.stringify(user))
+        window.location.href = "#";
+        console.log("deu boa");
+    }else{
+        document.getElementById("error-modal").style.display = "flex";
+        document.getElementById("user").style.border = "2px solid red";
+        document.getElementById("password").style.border = "2px solid red";
+    }
+}
+
+function closeError(){
+    document.getElementById("error-modal").style.display = "none";
+    document.getElementById("user").style.border = "2px solid rgba(235, 41, 206, 0.781)";
+    document.getElementById("password").style.border = "2px solid rgba(235, 41, 206, 0.781)";
+}
+
+function showPassword(){
+    var inputPassword = document.querySelector('#password');
+
+    if(inputPassword.getAttribute("type") === "password"){
+        inputPassword.setAttribute("type", "text")
+    }else{  
+        inputPassword.setAttribute("type", "password")
+    }
+} 
