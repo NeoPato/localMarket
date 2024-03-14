e7994334b6b132eebcd0325ce30df64a2aac0ccf
NeoPato <playersgpro@gmail.com>
2024-03-14 16:33:13 -0300

    coisa

--- localMarket/Carrinho/script.js
+++ localMarket/Carrinho/script.js
@@ -71,7 +71,7 @@ function gerarDocumentosWord(){
     link.href = URL.createObjectURL(blob);
     link.download = "carrinho.doc";
     link.click();
-    document.getElementById("pedido").style.display = "none"
+    document.getElementById("pedido").style.display = "block"
     
 }
 
